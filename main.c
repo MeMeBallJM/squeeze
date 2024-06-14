3b6ade738677ab41f5641480f84a4f35171471ce
MeMeBallJM <morton.l.joshua@gmail.com>
2024-06-13 21:38:03 -0400

    Get file name from user

--- main.c
+++ main.c
@@ -1,4 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+
 int main(int argc, char const *argv[])
 {
+    if (argc != 2)
+    {
+        printf("Expected file to compress/decompress\n");
+        return 1;
+    }
+
+    char const *file_name = argv[1];
+    if (access(file_name, F_OK) != 0)
+    {
+        printf("Couldn't find file %s\n", file_name);
+        return 1;
+    }
+
     return 0;
 }
